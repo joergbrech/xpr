f3e770162df600d7c16857f15544fdfd16af6361
Jan Kleinert <jan.kleinert@dlr.de>
2021-12-01 15:03:18 +0100

    best try yet, doesnt recurse

--- src/lib.rs
+++ src/lib.rs
@@ -9,197 +9,165 @@
 //! use xpr::*; 
 //!
 //! // An expression 
-//! let y = -Xpr::new(2)*Xpr::new(21);
+//! let y = -Xpr::new(2)*Xpr::new(-21);
+//! assert_eq!(y.eval(), Ok(42));
 //! ```
 
 use std::any::Any;
 use std::marker::PhantomData;
 use std::ops::{Neg, Mul};
 
-/// This represents an expression characterized only by its
-/// output type. It must be generated from an `Xpr` instance
-/// and allows downcasting
-struct Expr<'a ,O> {
-    expr: &'a dyn Any ,
-    phantom: PhantomData<O>
-}
-impl<'a, O> Expr<'a, O>
-where O: 'static
-{
-
-    /// given two operand types L and R, this function downcasts
-    /// an instance `Expr<O>` to a reference of its contained `Xpr<O,L,R>`
-    fn downcast<L,R>(&self) -> Option<&Xpr<O,L,R>>
-    where L: 'static,
-          R: 'static
-    {
-        self.expr.downcast_ref::<Xpr<O,L,R>>()
-    }
-
-    fn as_expression(&self) -> Expression<'a> {
-        Expression { expr: self.expr }
-    }
-}
-
-/// This represents any expression where both the output and 
-/// operand types have been erased. It must be generated from 
-/// an `Expr<O>` instance and allows downcasting
-pub struct Expression<'a> {
-    expr: &'a dyn Any
-}
-impl<'a> Expression<'a>
-{
-
-    /// given an output type, this function downcasts
-    /// an instance `Expression` to its contained `Expr<O>`
-    fn downcast<O>(&self) -> Option<&Expr<O>>
-    where O: 'static
-    {
-        self.expr.downcast_ref::<Expr<O>>()
-    }
-
-    /// given an output type O and two operand types
-    /// L and R, this function downcasts an instance
-    /// `Expression` to its contained `Xpr<O,L,R>`
-    fn to_xpr<O,L,R>(&self) -> Option<&Xpr<O,L,R>>
-    where O: 'static,
-          L: 'static,
-          R: 'static
-    {
-        match self.downcast::<O>() {
-            Some(x) => x.downcast::<L,R>(),
-            _ => None
-        }
-    }
-}
-
-/// The `Terminal` struct represents a leaf expression in an expression tree
-pub struct Terminal<T>(T);
-
-/// The `UnaryOp` represents any unary operation that takes an input parameter
-/// of type I and returns a O
-pub struct UnaryOp<'a,O,I>{
-    input: Box::<Expr<'a, I>>,
-    phantom: PhantomData<O>
-}
-impl<'a,O,I> UnaryOp<'a,O,I> {
-    pub fn new(input : Box<Expr<'a, I>>) -> Self
-    {
-        UnaryOp{ input, phantom: PhantomData }
-    }
-}
-
-/// This enum represents all supported unary operations that take an input
-/// of type I and return a O
-pub enum UnaryOps<'a,O,I> {
-    Neg(UnaryOp<'a,O,I>)
-}
-
-/// The `BinaryOp` represents any binary operation that takes input parameters
-/// of type Expr<L> and Expr<R> and returns a O
-pub struct BinaryOp<'a,O,L,R> {
-    left: Box::<Expr<'a,L>>,
-    right: Box::<Expr<'a,R>>,
-    phantom: PhantomData<O>
-}
-impl<'a,O,L,R> BinaryOp<'a,O,L,R> {
-    pub fn new(left : Box<Expr<'a,L>>, right: Box<Expr<'a,R>>) -> Self {
-        BinaryOp { left, right, phantom: PhantomData }
-    }
-}
-
-/// This enum represents all supported binary operations that take operands of
-/// type L and R and return an O
-pub enum BinaryOps<'a,O,L,R> {
-    Mul(BinaryOp<'a,O,L,R>)
-}
 
 /// The enum `Xpr` represents all supported expressions. It is parametrized by an 
 /// output type O, as well as the outputs types of all operands
-pub enum Xpr<'a,O,L,R> {
+#[derive(Debug)]
+pub enum Xpr<O,L,R> 
+{
     /// represents a leaf expression of type O
-    Terminal(Terminal<O>),
-    /// represents any unary operation that takes an L parameter and returns an O
-    UnaryOp(UnaryOps<'a,O,L>),
-    /// represents any binary operation that takes operands L and R and returns an O
-    BinaryOp(BinaryOps<'a,O,L,R>)
+    Terminal(O),
+    /// represents negation: o = -l
+    Neg(UnaryOp<O,L>),
+    /// represents multiplication: o = l*r
+    Mul(BinaryOp<O,L,R>)
 }
 
-impl<'a,O,L,R> Xpr<'a,O,L,R>
-where O: Mul<Output = O> + Neg<Output = O> + Copy + 'static,
+impl<O,L,R> Xpr<O,L,R>
+where O: 'static,
       L: 'static,
       R: 'static
 {
     /// evaluates the expression
-    pub fn eval(&self) -> Result<O,&str> {
-        self.transform(|x| {
-            // To Do: Need to downcast Box<dyn Expression> to Xpr<O,L,R>
-            match x.to_xpr::<O,L,R>() {
-                Some(&Xpr::Terminal(Terminal(v))) => Some(v),
+    pub fn eval(&self) -> Result<O,&str> 
+    where O: core::fmt::Debug + Copy + Neg<Output = O> + Mul<Output = O>  // <-- To Do: Does this even make any sense?
+    {
+        self.transform(& mut |x : Box<AnyXpr> | {
+            match x.as_xpr::<O,Void,Void>() {
+                Some(&Xpr::Terminal(v)) => Some(v),
                 _ => None
             }
         }).ok_or("Error evaluating expression")
     }
 
-    pub fn transform<F,T>(&self, mut f: F) -> Option<T>
-    where F: FnMut(Box<Expression>) -> Option<T>,
+    pub fn transform<F,T>(&self, f: &mut F) -> Option<T>
+    where F: FnMut(Box<AnyXpr>) -> Option<T>,
           T: Mul<Output = T> + Neg<Output = T>
     {
         // CASE 1/3: Match! return f(self)
-        if let Some(v) = f(Box::new(self.as_expression())) { return Some(v); };
-
+        if let Some(v) = f(Box::new(self.as_anyxpr())) { return Some(v); };
+        
         match self {
             // CASE 2/3: We have reached a leaf-expression, no match!
             Xpr::Terminal(_) => None, 
             // CASE 3/3: Recurse and apply operation to result
-            Xpr::UnaryOp(op) => match op {
-                UnaryOps::Neg(x) => { x.transform(f).map(|y| -y) }
+            Xpr::Neg(op) => { 
+                //l.as_xpr::<L,R>().expect("Shit").transform(f).map(|l| -l) 
+                op.transform(f).map(|l| -l)
             },
-            Xpr::BinaryOp(op) => match op {
-                BinaryOps::Mul(x) => { x.transform(f).map(|(a,b)| a*b) }
+            Xpr::Mul(op) => { 
+                op.transform(f).map(|(l,r)| l*r)
             }
         }
     }
 
-    fn as_expr(&self) -> Expr<O>
+    fn as_subxpr<'a>(&'a self) -> SubXprRef<'a, O>
     {
-        Expr { expr: self, phantom: PhantomData }
+        SubXprRef { expr: Box::new(self), phantom: PhantomData }
     }
 
-    fn as_expression(&self) -> Expression {
-        Expression { expr: self }
+    fn to_subxpr(self) -> SubXpr<O>
+    {
+        SubXpr { expr: Box::new(self), phantom: PhantomData }
+    }
+
+    fn as_anyxpr(&self) -> AnyXpr {
+        AnyXpr { expr: self }
     }
 }
 
-impl<'a,O> Xpr<'a,O,O,O>
+/// A zero sized type. This can be replaced by !, once stabilized
+#[derive(Debug)]
+pub enum Void {}
+
+impl<O> Xpr<O,Void,Void>
 {
     /// A factory function to create a terminal without specifying the two 
     /// arbitrary operand types of `Xpr`
     pub fn new(o: O) -> Self {
-        Xpr::Terminal(Terminal(o))
+        Xpr::Terminal(o)
     }
 }
 
-impl<'a,O,I> UnaryOp<'a,O,I>
+impl<I,X,Y,O> Neg for Xpr<I,X,Y> 
+where X: 'static,
+      Y: 'static,
+      I: 'static + Neg<Output = O>
+{
+    type Output = Xpr<O,I,Void>;
+
+    fn neg(self) -> Self::Output {
+        Xpr::Neg(UnaryOp::new(Box::new(self.to_subxpr())))
+    }
+}
+
+impl<R,A,B,L,X,Y> Mul<Xpr<R,A,B>> for Xpr<L,X,Y>
+where R: 'static,
+      A: 'static,
+      B: 'static,
+      L: 'static + Mul<R>,
+      X: 'static,
+      Y: 'static
+{
+    type Output = Xpr<<L as Mul<R>>::Output,L,R>;
+
+    fn mul(self, other: Xpr<R,A,B>) -> Self::Output  {
+        Xpr::Mul(BinaryOp::new(Box::new(self.to_subxpr()), Box::new(other.to_subxpr())))
+    }
+}
+
+/// The `UnaryOp` represents any unary operation that takes an input parameter
+/// of type I and returns a O
+#[derive(Debug)]
+pub struct UnaryOp<O,I>{
+    input: Box::<SubXpr<I>>,
+    phantom: PhantomData<O>
+}
+impl<O,I> UnaryOp<O,I> 
 where I: 'static
 {
-    fn transform<F,FO>(&self, mut f: F) -> Option<FO>
-    where F: FnMut(Box<Expression>) -> Option<FO>
+    fn new(input : Box<SubXpr<I>>) -> Self
     {
-        // To Do: Need to upcast dyn ExpressionWithOutput<Output = I> to Expression
-        f(Box::new(self.input.as_expression()))
+        UnaryOp{ input, phantom: PhantomData }
+    }
+
+    fn transform<F,R>(&self, mut f: F) -> Option<R>
+    where F: FnMut(Box<AnyXpr>) -> Option<R>
+    {
+        f(Box::new(self.input.as_anyxpr()))
     }
 }
 
-impl<'a,O,L,R> BinaryOp<'a,O,L,R>
+
+/// The `BinaryOp` represents any binary operation that takes input parameters
+/// of type Expr<L> and Expr<R> and returns a O
+#[derive(Debug)]
+pub struct BinaryOp<O,L,R> {
+    left: Box::<SubXpr<L>>,
+    right: Box::<SubXpr<R>>,
+    phantom: PhantomData<O>
+}
+impl<O,L,R> BinaryOp<O,L,R> 
 where L: 'static,
       R: 'static
 {
-    fn transform<F,FO>(&self, mut f: F) -> Option<(FO,FO)>
-    where F: FnMut(Box<Expression>) -> Option<FO>
+
+    fn new(left : Box<SubXpr<L>>, right: Box<SubXpr<R>>) -> Self {
+        BinaryOp { left, right, phantom: PhantomData }
+    }
+
+    fn transform<F,X>(&self, mut f: F) -> Option<(X,X)>
+    where F: FnMut(Box<AnyXpr>) -> Option<X>
     {
-        // To Do: Need to upcast to Expression
-        match (f(Box::new(self.left.as_expression())), f(Box::new(self.right.as_expression()))) 
+        match (f(Box::new(self.left.as_anyxpr())), f(Box::new(self.right.as_anyxpr()))) 
         {
             (Some(l), Some(r)) => Some((l,r)),
             _ => None
@@ -207,55 +175,192 @@ where L: 'static,
     }
 }
 
-// impl<I,X,Y,O> Neg for Xpr<I,X,Y> 
-// where X: 'static,
-//       Y: 'static,
-//       I: Neg<Output = O> + 'static
-// {
-//     type Output = Xpr<O,I,Y>;
+/// This represents an expression characterized only by its
+/// output type. It must be generated from an `Xpr` instance
+/// and allows downcasting
+#[derive(Debug)]
+pub struct SubXpr<O> {
+    expr: Box<dyn Any>,
+    phantom: PhantomData<O>
+}
+impl<O> SubXpr<O>
+where O: 'static
+{
 
-//     /// neg consumes an Xpr instances e and returns -e
-//     fn neg(self) -> Self::Output {
-//         Xpr::UnaryOp(UnaryOps::Neg(UnaryOp::new(Box::new(self))))
-//     }
-// }
+    /// given two operand types L and R, this function downcasts
+    /// an `SubXpr<O>` instance to a reference of its contained `Xpr<O,L,R>`
+    fn as_xpr<'a,L,R>(&'a self) -> Option<&'a Xpr<O,L,R>>
+    where L: 'static,
+          R: 'static
+    {
+        self.expr.downcast_ref::<Xpr<O,L,R>>()
+    }
 
-// impl<OL,X,Y,OR,R,O> Mul<R> for Xpr<OL,X,Y>
-// where R : Expr<OR> + 'static,
-//       OL: Mul<OR, Output = O> + 'static,
-//       X: 'static,
-//       Y: 'static
-// {
-//     type Output = Xpr::<O,OL,OR>;
-//     /// mul consumes two Xpr instances l and r and returns l*r
-//     fn mul(self, other: R) -> Self::Output {
-//         Xpr::BinaryOp(BinaryOps::Mul(BinaryOp::new(Box::new(self), Box::new(other))))
-//     }
-// }
+    fn as_anyxpr(&self) -> AnyXpr {
+        AnyXpr { expr: &*self.expr }
+    }
+}
 
+/// This represents an expression characterized only by its
+/// output type. It must be generated from an `Xpr` instance
+/// and allows downcasting
+struct SubXprRef<'a,O> {
+    expr: Box<&'a dyn Any>,
+    phantom: PhantomData<O>
+}
+impl<'a, O> SubXprRef<'a, O>
+where O: 'static
+{
 
-#[cfg(test)]
+    /// given two operand types L and R, this function downcasts
+    /// an instance `Expr<O>` to a reference of its contained `Xpr<O,L,R>`
+    fn as_xpr<L,R>(&'a self) -> Option<&'a Xpr<O,L,R>>
+    where L: 'static,
+          R: 'static
+    {
+        self.expr.downcast_ref::<Xpr<O,L,R>>()
+    }
+
+    fn as_anyxpr(&self) -> AnyXpr {
+        AnyXpr { expr: *self.expr }
+    }
+}
+
+/// This represents any expression where both the output and 
+/// operand types have been erased. It must be generated from 
+/// an `Expr<O>` instance and allows downcasting
+pub struct AnyXpr<'a> {
+    expr: &'a dyn Any
+}
+impl<'a> AnyXpr<'a>
+{
+    /// given an output type O and two operand types
+    /// L and R, this function downcasts an instance
+    /// `Expression` to its contained `Xpr<O,L,R>`
+    fn as_xpr<O,L,R>(&self) -> Option<&Xpr<O,L,R>>
+    where O: 'static,
+          L: 'static,
+          R: 'static
+    {
+        self.expr.downcast_ref::<Xpr<O,L,R>>()
+    }
+}
+
+#[cfg(test)] 
 mod tests {
     use super::*;
-    
+    use std::any;
+
+    fn type_name_of<T>(_: &T) -> &str {
+        any::type_name::<T>()
+    }
+
     #[test]
-    fn test_terminal() {
+    fn terminal_cast_xpr_subxpr() {
+
+        let x = Xpr::new(42);
+        assert_eq!(type_name_of(&x), any::type_name::<Xpr<i32,Void,Void>>());
+
+        // as_subxpr borrows from x and returns SubXprRef
+        let y = x.as_subxpr();
+        assert_eq!(type_name_of(&y), any::type_name::<SubXprRef<i32>>());
+        
+        // to_subxpr consumes x and returns SubXpr
+        let x = x.to_subxpr();
+        assert_eq!(type_name_of(&x), any::type_name::<SubXpr<i32>>());
+
+        // as_xpr downcasts and returns Option<Xpr<...>>
+        let y = x.as_xpr::<Void,Void>().unwrap();
+        assert_eq!(type_name_of(&y), any::type_name::<&Xpr<i32,Void,Void>>());
+
+    }
+
+    #[test]
+    fn terminal_cast_incremental_consumed() {
+        let x = Xpr::new(42);
+        let y = x.to_subxpr();
+        let z = y.as_anyxpr();
+        let w = z.as_xpr::<i32, Void, Void>().unwrap();
+        assert_eq!(type_name_of(&w), any::type_name::<&Xpr<i32,Void,Void>>());
+        assert!(matches!(w, Xpr::<i32,Void,Void>::Terminal(m) if *m==42 ));
+    }
+
+    #[test]
+    fn terminal_cast_incremental_referenced() {
+        let x = Xpr::new(42);
+        let y = x.as_subxpr();
+        let z = y.as_anyxpr();
+        let w = z.as_xpr::<i32, Void, Void>().unwrap();
+        assert_eq!(type_name_of(&w), any::type_name::<&Xpr<i32,Void,Void>>());
+        assert!(matches!(w, Xpr::<i32,Void,Void>::Terminal(m) if *m==42 ));
+    }
+
+    #[test]
+    fn terminal_cast_xpr_anyxpr() {
+
         let x = Xpr::new(42);
 
-        // let ret = x.transform(&mut |e| {
+        // as_anyxpr returns AnyXpr
+        let y = x.as_anyxpr();
+        assert_eq!(type_name_of(&y), any::type_name::<AnyXpr>());
 
 
-        //     // transform borrows f which is FnMut as &mut
-        //     // f owns Box of borrowed x as e which is Box<&dyn Expression>
-        //     // reference to `dyn Expression` is not 'static
+        let y = y.as_xpr::<i32,Void,Void>().unwrap();
+        assert_eq!(type_name_of(&y), any::type_name::<&Xpr<i32,Void,Void>>());
 
-        //     // first check if it is a terminal
-        //     match expression_cast::<i32>(*e) {
-        //         Some(Xpr::Term(Terminal(_))) => Some(0),
-        //         Some(Xpr::Ops(Ops::Neg(z))) => Some(1),
-        //         _ => None
-        //     }
-        // }
-        // );
+    }
+
+    #[test]
+    fn terminal_cast_subxpr_anyxpr(){
+
+        let x = Xpr::new(42).to_subxpr();
+
+        // as_anyxpr returns AnyXpr
+        let x = x.as_anyxpr();
+        assert_eq!(type_name_of(&x), any::type_name::<AnyXpr>());
+    }
+    
+    #[test]
+    fn terminal_eval_vs_transform() {
+        
+        let mut evaluator = |e : Box<AnyXpr> | {
+            match e.as_xpr::<i32,Void,Void>() {
+                Some(&Xpr::Terminal(v)) => Some(v),
+                _ => None
+            }
+        };
+
+        let x = Xpr::new(2);
+        assert_eq!(x.transform(&mut evaluator), Some(2));
+        assert_eq!(x.eval(), Ok(2));
+    }
+
+    #[test]
+    fn mul_eval_vs_transform() {
+
+        let mut evaluator = |e : Box<AnyXpr> | {
+            match e.as_xpr::<i32,Void,Void>() {
+                Some(&Xpr::Terminal(v)) => Some(v),
+                _ => None
+            }
+        };
+
+        let x = Xpr::new(2)*Xpr::new(21);
+        assert_eq!(x.transform(&mut evaluator), Some(42));
+        assert_eq!(x.eval(), Ok(42));
+    }
+
+    #[test]
+    fn neg_eval_vs_transform() {
+        let mut evaluator = |e : Box<AnyXpr> | {
+            match e.as_xpr::<i32,Void,Void>() {
+                Some(&Xpr::Terminal(v)) => Some(v),
+                _ => None
+            }
+        };
+
+        let x = -Xpr::new(2);
+        assert_eq!(x.transform(&mut evaluator), Some(-2));
+        assert_eq!(x.eval(), Ok(-2));
     }
 }
