94eadc14834391be430a7187a1c2ec4f72c68a07
Jan Kleinert <jan.kleinert@dlr.de>
2021-12-07 10:22:29 +0100

    first try with fold

--- src/lib.rs
+++ src/lib.rs
@@ -7,378 +7,227 @@
 //! # Example usage
 //! ```rust
 //! use xpr::*;
+//! 
+//! // create a folder, which is any struct implementing `Fold`
+//! struct Folder;
+//! impl Fold for Folder {
+//!     type Output = i32;
+//!     fn fold<T>(&mut self, e: &Xpr<T>) -> Option<Self::Output> {
+//!         match e {
+//!             Xpr::Term(_) => { println!("I am a terminal!"); Some(42) },
+//!             Xpr::Neg(_) =>  { println!("I am a negation!"); None },
+//!             Xpr::Add(_) =>  { println!("I am an addition!"); Some(41) },
+//!             Xpr::Mul(_) =>  { println!("I am a multiplication!"); None },
+//!         }
+//!     }
+//! }
 //!
-//! // An expression
-//! let y = -Xpr::Terminal(2)*Xpr::Terminal(-21);
-//! assert_eq!(y.eval(), Ok(42));
+//! // create an expression
+//! let x = -Xpr::new(5)*(Xpr::new(7) + Xpr::new(-10));
+//! println!("{:?}",x)
+//! 
+//! x.transform(&Folder{});
 //! ```
 
-use std::any::Any;
-use std::marker::PhantomData;
 use std::ops;
+use std::fmt;
 
-/// A trait for evaluating expressions
-pub trait Eval {
-    /// evaluate an expression. This is syntactic sugar around a transform, that unwraps any terminal
-    /// to its contained type, see [`Transform::transform`].
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// use xpr::*;
-    ///
-    /// // An expression
-    /// let y = -Xpr::Terminal(3)*Xpr::Terminal(2)*Xpr::Terminal(-7);
-    ///
-    /// assert_eq!(y.eval(), Ok(42));
-    /// ```
-    fn eval<R>(&self) -> Result<R, &str>
-    where
-        Self: Sized,
-        R: 'static + Copy;
-}
-
-/// A trait for transforming expressions
-pub trait Transform: TransformInternal {
-    /// The [`Transform::transform`] function is a central feature of this crate. It can be used to transform and manipulate
-    /// expression by traversing the expression tree made up of an expression's subexpressions.
-    ///
-    ///
-    /// It takes a mutable visiting closure that returns an [`Option<T>`]. The expression tree is traversed
-    /// recursively until the closure returns `Some(_)`, in which case the return value is passed upwards through the
-    /// expression tree, applying each operation to the value contained in the `Some` on the way.
-    ///
-    /// Note that the argument to the visitor closure `f` is a to [`&dyn Any`]. This way, it can be applied to any kind
-    /// of expression. To properly match expressions in the body of `f`, we need to downcast the argument to an `&Xpr<T>`
-    /// with an appropriate return type `T` using `downcast_ref`, see the examples below.
-    ///
-    /// # Examples
-    ///
-    /// ## A transform that returns a value
-    ///
-    /// This example demonstrates how an expression can be evaluated using a transform. This is taken directly from the
-    /// implementation of [`Eval::eval`]. We will match any terminal, unwrap its wrapped value and pass that up the the
-    /// expression tree, evaluating each subexpression along the way.
-    ///
-    /// ```rust
-    /// use xpr::*;
-    /// use std::any::Any;
-    ///
-    /// /// triple negation
-    /// let x = ---Xpr::Terminal(42);
-    ///
-    /// /// the following transform evaluates an expression
-    /// let mut evaluator = |e: &dyn Any| -> Option<i32> {
-    ///     match e.downcast_ref::<Xpr<i32>>() {
-    ///         Some(&Xpr::Terminal(x)) => Some(x),
-    ///         _ => None
-    ///     }
-    /// };
-    ///
-    /// let result = x.transform::<i32>(&mut evaluator);
-    ///
-    /// assert_eq!(result, Some(-42));
-    /// ```
-    ///
-    /// Note that the type annotations can be dropped if the closure is passed anonymously to the function, as can be seen in the next
-    /// example
-    ///
-    /// ## A transform that always returns `None`
-    ///
-    /// In this example we are not interested in transforming the expression, we just want to do some analysis on it.
-    /// Let's count the number of negations from the expression of the previous example
-    ///
-    /// ```rust
-    /// use xpr::*;
-    ///
-    /// // create an expression
-    /// let y = ---Xpr::Terminal(42);
-    ///
-    /// let mut count = 0;
-    ///
-    /// // traverse the expression tree and capture count mutably
-    /// y.transform(&mut |x| -> Option<()> {
-    ///     match x.downcast_ref::<Xpr<i32>>() {
-    ///         Some(&Xpr::Neg(_)) => {
-    ///             count+=1;
-    ///             None
-    ///         }
-    ///         _ => None
-    ///     }
-    /// });
-    ///
-    /// assert_eq!(count, 3);
-    /// ```
-    ///
-    /// We still need to directly declare the output type, because the closure never returns `Some`.
-    /// In the next example, the compiler will be able to deduce the output type for us.
-    ///
-    /// ## A transform that returns an expression
-    ///
-    /// This example demonstrates how to replace a terminal expression with another expression.
-    ///
-    /// **To Do**
-    ///
-    /// ```rust
-    /// use xpr::*;
-    ///
-    /// let x = -Xpr::Terminal(5)*Xpr::Terminal(6);
-    /// let y = Xpr::Terminal(-2)*Xpr::Terminal(7);
-    ///
-    /// //let z = x.transform(&mut |e|{
-    /// //    match e.as_xpr::<i32>() {
-    /// //        // replaces 5 with 3
-    /// //        Some(&Xpr::Terminal(v)) if v==5 => Some(Xpr::Terminal(3)),
-    /// //        // inserts y in place of 6
-    /// //        Some(&Xpr::Terminal(v)) => Some(y),
-    /// //        _ => None
-    /// //    }
-    /// //});
-    /// //
-    /// //assert_eq!(z.unwrap().eval(), Ok(42));
-    /// ```
-    ///
-    fn transform<R>(&self, f: &mut dyn FnMut(&dyn Any) -> Option<R>) -> Option<R>
-    where
-        R: 'static + Copy,
+pub trait Transform<F>
+where F: Fold
+{
+    type Output;
+    fn transform(&self, _: &F) -> Option<Self::Output>
     {
-        let mut t = |e: &dyn Any| -> Option<Box<dyn Any>> {
-            match f(e) {
-                Some(e) => Some(Box::new(e)),
-                _ => None,
-            }
-        };
-        cast_optional_any(self.transform_internal(&mut t))
+        None
     }
 }
 
-mod private {
-    use std::any::Any;
+pub trait Fold
+{
+    type Output;
+    fn fold<T>(&mut self, _: &Xpr<T>) -> Option<Self::Output>;
+}
 
-    pub trait TransformInternal {
-        /// The [`Transform::transform_internal`] function can be used to transform and manipulate
-        /// expression by traversing the expression tree made up of an expression's subexpressions.
-        ///
-        /// The return type of [`Transform::transform`] is an [`std::any::Any`]. For now the type erasure of
-        /// the return type is needed to properly traverse the tree. [`Xpr::transform`] is a more convenient version
-        /// of this transform.
-        ///
-        /// The type erased return type is a bit misfortunate, because
-        /// 1. I cannot replace an operation taking in input of type X with an output of type Y, because the operation
-        ///    must downcast to an `Xpr<X>` when passing the returned value up through the expression tree.
-        /// 2. It makes the code a bit awkward.
-        ///
-        /// It is currently needed, for the following reason:
-        /// 1. I need the transform(_internal) function to be a trait method, because I want to store trait objects as operands
-        ///    to expressions. This way I have shared behavior (all operands can transform recursively)
-        /// 2. I need some form of type erasure of the operands of the operands.
-        ///
-        /// Ideally, the transform(_internal) would return `Option<T>` for a generic `T`, and we wouldn't need the extra level
-        /// of indirection between [`Xpr::transform`] and [`Transform::transform_internal`], but I can't call generic trait
-        /// methods on trait objects.
-        ///
-        fn transform_internal(
-            &self,
-            f: &mut dyn FnMut(&dyn Any) -> Option<Box<dyn Any>>,
-        ) -> Option<Box<dyn Any>>;
+#[derive(Clone,Copy,Debug)]
+pub enum Xpr<T> {
+    Term(T),
+    Neg(T),
+    Add(T),
+    Mul(T)
+}
+
+impl<T> Xpr<XprTerm<T>> {
+    pub fn new(t: T) -> Self {
+        Xpr::Term(XprTerm(Box::new(t)))
     }
+}
 
-    /// A helper function to downcast a Option<Box<dyn Any>> to an Option<T>
-    pub fn cast_optional_any<T>(x: Option<Box<dyn Any>>) -> Option<T>
-    where
-        T: 'static + Copy,
+impl<T,F> Transform<F> for Xpr<T>
+where 
+T: Transform<F>,
+F: Fold
+{
+    type Output = <T as Transform<F>>::Output;
+    fn transform(&self, f: &F) -> Option<Self::Output>
     {
-        match x {
-            Some(x) => x.downcast_ref::<T>().copied(),
-            _ => None,
-        }
-    }
-}
-use private::*;
-
-impl<T> Transform for T where T: TransformInternal {}
-
-impl<T> Eval for T
-where
-    T: Transform,
-{
-    fn eval<R: 'static + Copy>(&self) -> Result<R, &str> {
-        let mut evaluator = |e: &dyn Any| -> Option<R> {
-            match e.downcast_ref::<Xpr<R>>() {
-                Some(&Xpr::Terminal(x)) => Some(x),
-                _ => None,
-            }
-        };
-
-        self.transform(&mut evaluator)
-            .ok_or("Error evaluation expression")
-    }
-}
-
-//***********************************************************************//
-
-/// Supported expression types.
-///
-/// Note that [`Xpr::Terminal`] is the only variant that can be instantiated
-/// explicitly. It represents the leaf expressions of any expression. All
-/// other expressions are created by applying the supported operations to
-/// `Xpr::Terminal` instances.
-///
-pub enum Xpr<T>
-where
-    T: 'static + Copy,
-{
-    /// A Terminal represents a leaf expression, e.g. a single value
-    Terminal(T),
-    /// Negation of an expression `l -> -l`
-    Neg(Box<dyn TransformInternal>),
-    /// Multiplication of two expressions `(l,r) -> l*r`
-    Mul(Box<dyn TransformInternal>),
-}
-
-impl<T> TransformInternal for Xpr<T>
-where
-    T: 'static + Copy,
-{
-    fn transform_internal(
-        &self,
-        f: &mut dyn FnMut(&dyn Any) -> Option<Box<dyn Any>>,
-    ) -> Option<Box<dyn Any>> {
-        if let Some(v) = f(self) {
-            return Some(v);
-        }
+        //if let Some(v) = f.fold(self) { return Some(v); }
 
         match self {
-            Xpr::Terminal(_) => None,
-            Xpr::Mul(x) => x.transform_internal(f),
-            Xpr::Neg(x) => x.transform_internal(f),
+            Xpr::Term(x) => x.transform(f),
+            Xpr::Neg(x) => x.transform(f),
+            Xpr::Add(x) => x.transform(f),
+            Xpr::Mul(x) => x.transform(f)
         }
     }
 }
 
-//***********************************************************************//
-
-struct Neg<T> {
-    input: Box<dyn TransformInternal>,
-    _input: PhantomData<T>,
-}
-impl<T> Neg<T> {
-    fn new(input: Box<dyn TransformInternal>) -> Self {
-        Neg {
-            input,
-            _input: PhantomData::<T>,
-        }
-    }
-}
-impl<T> TransformInternal for Neg<T>
-where
-    T: 'static + Copy + ops::Neg,
+pub struct XprTerm<T>(Box<T>);
+impl<T,F> Transform<F> for XprTerm<T> 
+where F: Fold
 {
-    fn transform_internal(
-        &self,
-        f: &mut dyn FnMut(&dyn Any) -> Option<Box<dyn Any>>,
-    ) -> Option<Box<dyn Any>> {
-        if let Some(l) = cast_optional_any::<T>(self.input.transform_internal(f)) {
-            return Some(Box::new(-l));
-        }
-        None
-    }
+    type Output = <F as Fold>::Output;
 }
-
-impl<T> ops::Neg for Xpr<T>
-where
-    T: 'static + Copy + ops::Neg,
-    <T as ops::Neg>::Output: Copy,
+impl<T> fmt::Debug for XprTerm<T>
+where 
+    T: fmt::Debug
 {
-    type Output = Xpr<<T as ops::Neg>::Output>;
-    fn neg(self) -> Self::Output {
-        Xpr::Neg(Box::new(Neg::<T>::new(Box::new(self))))
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
     }
 }
 
-//***********************************************************************//
-
-struct Mul<L, R> {
-    left: Box<dyn TransformInternal>,
-    right: Box<dyn TransformInternal>,
-    _left: PhantomData<L>,
-    _right: PhantomData<R>,
+pub struct XprNeg<T>{
+    input: Box<T>
 }
-impl<L, R> Mul<L, R> {
-    fn new(left: Box<dyn TransformInternal>, right: Box<dyn TransformInternal>) -> Self {
-        Mul {
-            left,
-            right,
-            _left: PhantomData::<L>,
-            _right: PhantomData::<R>,
-        }
-    }
-}
-impl<L, R> TransformInternal for Mul<L, R>
-where
-    L: 'static + Copy + ops::Mul<R>,
-    R: 'static + Copy,
-    <L as ops::Mul<R>>::Output: 'static,
+impl<T,F> Transform<F> for XprNeg<T> 
+where 
+    T: Transform<F>,
+    F: Fold,
+    <T as Transform<F>>::Output: ops::Neg
 {
-    fn transform_internal(
-        &self,
-        f: &mut dyn FnMut(&dyn Any) -> Option<Box<dyn Any>>,
-    ) -> Option<Box<dyn Any>> {
-        if let Some(l) = cast_optional_any::<L>(self.left.transform_internal(f)) {
-            if let Some(r) = cast_optional_any::<R>(self.right.transform_internal(f)) {
-                return Some(Box::new(l * r));
+    type Output = <<T as Transform<F>>::Output as ops::Neg>::Output;
+    fn transform(&self, f: &F) -> Option<Self::Output>
+    {
+        self.input.transform(f).map(|x| -x)
+    }
+}
+impl<T> fmt::Debug for XprNeg<T>
+where T: fmt::Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.input.fmt(f)
+    }
+}
+
+pub struct XprAdd<T>{ 
+    operands: Box<T>
+}
+impl<L,R,F> Transform<F> for XprAdd<(L,R)> 
+where
+    L: Transform<F>,
+    R: Transform<F>,
+    F: Fold,
+    <L as Transform<F>>::Output: ops::Add<<R as Transform<F>>::Output>
+{
+    type Output = <<L as Transform<F>>::Output as ops::Add<<R as Transform<F>>::Output>>::Output;
+    fn transform(&self, f: &F) -> Option<Self::Output>
+    {
+        if let Some(l) = self.operands.0.transform(f) {
+            if let Some(r) = self.operands.1.transform(f) {
+                return Some(l+r);
             }
         }
         None
     }
 }
-
-impl<L, R> ops::Mul<Xpr<R>> for Xpr<L>
-where
-    L: 'static + Copy + ops::Mul<R>,
-    R: 'static + Copy,
-    <L as ops::Mul<R>>::Output: Copy,
+impl<L,R> fmt::Debug for XprAdd<(L,R)>
+where 
+    L: fmt::Debug,
+    R: fmt::Debug
 {
-    type Output = Xpr<<L as ops::Mul<R>>::Output>;
-    fn mul(self, other: Xpr<R>) -> Self::Output {
-        Xpr::Mul(Box::new(Mul::<L, R>::new(Box::new(self), Box::new(other))))
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("")
+         .field(&self.operands.0)
+         .field(&self.operands.1)
+         .finish()
     }
 }
 
-//***********************************************************************//
+pub struct XprMul<T>{ 
+    operands: Box<T>
+}
+impl<L,R,F> Transform<F> for XprMul<(L,R)>
+where 
+    L: Transform<F>,
+    R: Transform<F>,
+    F: Fold,
+    <L as Transform<F>>::Output: ops::Mul<<R as Transform<F>>::Output>
+{
+    type Output = <<L as Transform<F>>::Output as ops::Mul<<R as Transform<F>>::Output>>::Output;
+    fn transform(&self, f: &F) -> Option<Self::Output>
+    {
+        if let Some(l) = self.operands.0.transform(f) {
+            if let Some(r) = self.operands.1.transform(f) {
+                return Some(l*r)
+            }
+        }
+        None
+    }
+}
+impl<L,R> fmt::Debug for XprMul<(L,R)>
+where 
+    L: fmt::Debug,
+    R: fmt::Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("")
+         .field(&self.operands.0)
+         .field(&self.operands.1)
+         .finish()
+    }
+}
+
+// implement operations for Xpr
+
+impl<L> ops::Neg for Xpr<L> {
+    type Output = Xpr<XprNeg<Xpr<L>>>;
+    fn neg(self) -> Self::Output {
+        Xpr::Neg(XprNeg{ input: Box::new(self)})
+    }
+}
+
+impl<L,R> ops::Add<Xpr<R>> for Xpr<L> {
+    type Output = Xpr<XprAdd<(Xpr<L>,Xpr<R>)>>;
+    fn add(self, other: Xpr<R>) -> Self::Output {
+        Xpr::Add(XprAdd{ operands: Box::new((self,other))})
+    }
+}
+
+impl<L,R> ops::Mul<Xpr<R>> for Xpr<L> {
+    type Output = Xpr<XprMul<(Xpr<L>,Xpr<R>)>>;
+    fn mul(self, other: Xpr<R>) -> Self::Output {
+        Xpr::Mul(XprMul{ operands: Box::new((self,other))})
+    }
+}
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests
+{
+    // use super::*;
 
-    #[test]
-    fn test_mul_eval_vs_transform() {
-        let mut evaluator = |e: &dyn Any| -> Option<i32> {
-            match e.downcast_ref::<Xpr<i32>>() {
-                Some(&Xpr::Terminal(x)) => Some(x),
-                _ => None,
-            }
-        };
+    // struct Evaluator;
+    // impl Fold for Evaluator {
+    //     type Output = i32;
+    //     fn fold<i32>(&mut self, e: &Xpr<i32>) -> Option<Self::Output> {
+    //         match e {
+    //             Xpr::Term(x) => { Some(x) },
+    //             _ => None
+    //         }
+    //     }
+    // }
 
-        let x = Xpr::Terminal(3) * Xpr::Terminal(2) * Xpr::Terminal(7);
-        assert_eq!(x.eval(), Ok(42));
-        assert_eq!(x.transform::<i32>(&mut evaluator), Some(42))
-    }
+    // #[test]
+    // fn test() {
 
-    // a struct that doesn't implement all supported operations
-    #[derive(Clone, Copy, Debug, PartialEq)]
-    struct OnlyNeg(i32);
-
-    impl ops::Neg for OnlyNeg {
-        type Output = Self;
-        fn neg(self) -> Self::Output {
-            OnlyNeg(-self.0)
-        }
-    }
-
-    #[test]
-    fn test_neg_only() {
-        let x = -Xpr::Terminal(OnlyNeg(1));
-        assert_eq!(x.eval(), Ok(OnlyNeg(-1)));
-    }
+    // }
 }
