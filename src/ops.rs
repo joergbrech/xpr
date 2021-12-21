f85c27d6f791f003004b01860780efa55928246f
Jan Kleinert <jan.kleinert@dlr.de>
2021-12-21 12:12:45 +0100

    shorter docstrings for binary ops

--- src/ops.rs
+++ src/ops.rs
@@ -25,16 +25,16 @@ where
 }
 
 // expand implementations for binary operations
-binary_op!(Add, add, +, OutputOfAdd, "An `Xpr<Add<L,R>>` represents the addition of two operands `l: L` and `r: R`: `l + r`");
-binary_op!(Sub, sub, -, OutputOfSub, "An `Xpr<Sub<L,R>>` represents the subtraction of `r: R` from `l: L`: `l - r`");
-binary_op!(Mul, mul, *, OutputOfMul, "An `Xpr<Mul<L,R>>` represents the multiplication of two operands `l: L` and `r: R`: l * r");
-binary_op!(Div, div, /, OutputOfDiv, "An `Xpr<Div<L,R>>` represents the division of `l: L` by `r: R`: l / r");
-binary_op!(Rem, rem, %, OutputOfRem, "An `Xpr<Rem<L,R>>` represents the remainder of `l: L` after devision by `r: R`: l % r");
-binary_op!(Shl, shl, <<, OutputOfShl, "An `Xpr<Shl<L,R>>` represents the left shift operator of `l: L` by `r: R`: l << r");
-binary_op!(Shr, shr, >>, OutputOfShr, "An `Xpr<Shr<L,R>>` represents the right shift operator of `l: L` by `r: R`: l >> r");
-binary_op!(BitAnd, bitand, &, OutputOfBitAnd, "An `Xpr<BitAnd<L,R>>` represents the bitwise AND of two operands `l: L` and `r: R`: `l & r`");
-binary_op!(BitOr, bitor, |, OutputOfBitOr, "An `Xpr<BitOr<L,R>>` represents the bitwise OR of two operands `l: L` and `r: R`: `l | r`");
-binary_op!(BitXor, bitxor, ^, OutputOfBitXor, "An `Xpr<BitXor<L,R>>` represents the bitwise XOR of two operands `l: L` and `r: R`: `l ^ r`");
+binary_op!(Add, add, +, OutputOfAdd, "An `Xpr<Add<L,R>>` represents addition: `l + r`");
+binary_op!(Sub, sub, -, OutputOfSub, "An `Xpr<Sub<L,R>>` represents subtractio: `l - r`");
+binary_op!(Mul, mul, *, OutputOfMul, "An `Xpr<Mul<L,R>>` represents multiplication: l * r");
+binary_op!(Div, div, /, OutputOfDiv, "An `Xpr<Div<L,R>>` represents division: l / r");
+binary_op!(Rem, rem, %, OutputOfRem, "An `Xpr<Rem<L,R>>` represents remainder: R`: l % r");
+binary_op!(Shl, shl, <<, OutputOfShl, "An `Xpr<Shl<L,R>>` represents the left shift operator: R`: l << r");
+binary_op!(Shr, shr, >>, OutputOfShr, "An `Xpr<Shr<L,R>>` represents the right shift operator: R`: l >> r");
+binary_op!(BitAnd, bitand, &, OutputOfBitAnd, "An `Xpr<BitAnd<L,R>>` represents the bitwise AND: R`: `l & r`");
+binary_op!(BitOr, bitor, |, OutputOfBitOr, "An `Xpr<BitOr<L,R>>` represents the bitwise OR: L` and `r: R`: `l | r`");
+binary_op!(BitXor, bitxor, ^, OutputOfBitXor, "An `Xpr<BitXor<L,R>>` represents the bitwise XOR: R`: `l ^ r`");
 
 
 // To Do
